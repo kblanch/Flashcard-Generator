3b00e07311ce17a1ec0c44466252060e87b021f4
Karen <K.Blanchette07@gmail.com>
2017-07-24 23:36:30 -0500

    Basic Card

---
+++ ClozeCard.js
@@ -0,0 +1,8 @@
+var ClozeCard = function(text,cloze){
+    this.fullText = text;
+    this.cloze = cloze;
+    //this.partial = ;
+    //Throw or log error if Cloze deletion does not appear in the input text
+};
+
+module.exports = ClozeCard;
